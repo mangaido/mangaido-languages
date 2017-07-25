f274a333cdf3bd95066fb1fc7454c6da070e48bf
Michał Wrzosek <michal@wrzosek.pl>
2017-07-25 17:04:48 +0200

    Added Filipino and fixed some small bugs in Spanish files

---
+++ pluralization/es.rb
@@ -0,0 +1,21 @@
+# https://github.com/svenfuchs/rails-i18n/blob/master/rails/pluralization/es.rb
+
+key = lambda do |n|
+  n ||= 0
+
+  if n == 1
+    :one
+  else
+    :other
+  end
+end
+
+{ :es => {
+    :'i18n' => {
+      :plural => {
+        :keys => [:one, :other],
+        :rule => key
+      }
+    }
+  }
+}
--- pluralization/tl.rb
+++ pluralization/tl.rb
@@ -1,16 +1,16 @@
-# https://github.com/svenfuchs/rails-i18n/blob/master/rails/pluralization/es.rb
+# https://github.com/svenfuchs/rails-i18n/blob/master/rails/pluralization/tl.rb
 
 key = lambda do |n|
   n ||= 0
 
-  if n == 1
+  if n == 0 || n == 1
     :one
   else
     :other
   end
 end
 
-{ :en => {
+{ :tl => {
     :'i18n' => {
       :plural => {
         :keys => [:one, :other],
